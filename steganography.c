4f60c93f5c515ccc6473b20e6ea9c94cb26afaa7
qianjindexiaozu <2899094864@qq.com>
2024-03-05 14:21:00 +0800

    Part A2 has down

--- steganography.c
+++ steganography.c
@@ -8,9 +8,9 @@
 **              Copyright (C) Dan Garcia, 2020. All rights reserved.
 **				Justin Yokota - Starter Code
 **				YOUR NAME HERE
-**
+**				Andy Wang
 ** DATE:        2020-08-23
-**
+**				2024-3-5
 **************************************************************************/
 
 #include <stdio.h>
@@ -22,12 +22,37 @@
 Color *evaluateOnePixel(Image *image, int row, int col)
 {
 	//YOUR CODE HERE
+	int place = row * image->cols + col;
+	Color *new_color = (Color *)malloc(sizeof(Color));
+	if((*(image->image + place))->B % 2){
+		new_color->R = 255;
+		new_color->G = 255;
+		new_color->B = 255;
+	}
+	else {
+		new_color->R = 0;
+		new_color->G = 0;
+		new_color->B = 0;
+	}
+	return new_color;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
 	//YOUR CODE HERE
+	Image* new_image = (Image *)malloc(sizeof(Image));
+	new_image->image = (Color **)malloc(sizeof(Color*) * image->cols * image->rows);
+	new_image->cols = image->cols;
+	new_image->rows = image->rows;
+	Color **p = new_image->image;
+	for(int i = 0; i < image->rows; i++){
+		for(int j = 0; j < image->cols; j++){
+			*p = evaluateOnePixel(image, i, j);
+			p++;
+		}
+	}
+	return new_image;
 }
 
 /*
@@ -46,4 +71,23 @@ Make sure to free all memory before returning!
 int main(int argc, char **argv)
 {
 	//YOUR CODE HERE
+	if(argc != 2){
+		printf("argument error!\n");
+		exit(-1);
+	}
+	Image* startedImage = readData(argv[1]);
+	if(!startedImage){
+		printf("read date error!\n");
+		exit(-1);
+	}
+	Image* steganographyedImage = steganography(startedImage);
+	if(!steganographyedImage){
+		printf("steganography error!\n");
+		exit(-1);
+	}
+	writeData(steganographyedImage);
+	freeImage(startedImage);
+	freeImage(steganographyedImage);
+	return 0;
+
 }
