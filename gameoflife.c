3dbbf97b098eacc176b60d2af9840ece72d10a42
qianjindexiaozu <2899094864@qq.com>
2024-03-05 16:06:16 +0800

    Part B has down

--- gameoflife.c
+++ gameoflife.c
@@ -6,10 +6,10 @@
 **
 ** AUTHOR:      Justin Yokota - Starter Code
 **				YOUR NAME HERE
-**
+**				Andy Wang
 **
 ** DATE:        2020-08-23
-**
+**				2024-03-05
 **************************************************************************/
 
 #include <stdio.h>
@@ -23,6 +23,50 @@
 Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
 {
 	//YOUR CODE HERE
+	int place = row * image->cols + col;
+	Color *new_color = (Color *)malloc(sizeof(Color));
+	int countR = 0, countG = 0, countB = 0, nowR = 0, nowG = 0, nowB = 0;
+
+	if((*(image->image + place))->R)	nowR = 1;
+	if((*(image->image + place))->G)	nowG = 1;
+	if((*(image->image + place))->B)	nowB = 1;
+
+	for(int i = -1; i < 2; i++){
+		for(int j = -1; j < 2; j++){
+			if(i == 0 && j == 0) continue;
+			int temp = (i + row + image->rows) % image->rows * image->cols + (col + j + image->cols) % image->cols;
+			if((*(image->image + temp))->R) countR++;
+			if((*(image->image + temp))->G) countG++;
+			if((*(image->image + temp))->B) countB++;
+		}
+	}
+
+
+	uint32_t state_R = 1, state_G = 1, state_B = 1;
+	state_R = state_R << (nowR * 9 + countR);
+	state_G = state_G << (nowG * 9 + countG);
+	state_B = state_B << (nowB * 9 + countB);
+	if(state_R & rule){
+		new_color->R = 255;
+	}
+	else {
+		new_color->R = 0;
+	}
+	if(state_G & rule){
+		new_color->G = 255;
+	}
+	else {
+		new_color->G = 0;
+	}
+	if(state_B & rule){
+		new_color->B = 255;
+	}
+	else {
+		new_color->B = 0;
+	}
+	return new_color;
+
+	
 }
 
 //The main body of Life; given an image and a rule, computes one iteration of the Game of Life.
@@ -30,6 +74,18 @@ Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
 Image *life(Image *image, uint32_t rule)
 {
 	//YOUR CODE HERE
+	Image* new_image = (Image *)malloc(sizeof(Image));
+	new_image->image = (Color **)malloc(sizeof(Color*) * image->cols * image->rows);
+	new_image->cols = image->cols;
+	new_image->rows = image->rows;
+	Color **p = new_image->image;
+	for(int i = 0; i < image->rows; i++){
+		for(int j = 0; j < image->cols; j++){
+			*p = evaluateOneCell(image, i, j, rule);
+			p++;
+		}
+	}
+	return new_image;
 }
 
 /*
@@ -50,4 +106,24 @@ You may find it useful to copy the code from steganography.c, to start.
 int main(int argc, char **argv)
 {
 	//YOUR CODE HERE
+	if(argc != 3 || argv[2][0] != '0' || argv[2][1] != 'x'){
+		printf("usage: ./gameOfLife filename rule\nfilename is an ASCII PPM file (type P3) with maximum value 255.\nrule is a hex number beginning with 0x; Life is 0x1808.\n");
+		exit(-1);
+	}
+	uint32_t rule = strtol(argv[2], NULL, 16);
+	Image* startedImage = readData(argv[1]);
+	if(!startedImage){
+		printf("read date error!\n");
+		exit(-1);
+	}
+	Image* lifeImage = life(startedImage, rule);
+	if(!lifeImage){
+		printf("life error!\n");
+		exit(-1);
+	}
+	writeData(lifeImage);
+	freeImage(startedImage);
+	freeImage(lifeImage);
+	return 0;
+
 }
